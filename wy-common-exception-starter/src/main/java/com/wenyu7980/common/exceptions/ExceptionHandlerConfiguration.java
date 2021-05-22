dae2c82f4df1bcd229cdc2e15662324c0de24099
wenyu <wenyu7980@163.com>
2021-05-22 16:49:07 +0800

    1.删除context 2.修改pom

--- wy-common-exception-starter/src/main/java/com/wenyu7980/common/exceptions/ExceptionHandlerConfiguration.java
+++ wy-common-exception-starter/src/main/java/com/wenyu7980/common/exceptions/ExceptionHandlerConfiguration.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -39,6 +40,9 @@ public class ExceptionHandlerConfiguration {
               .map(error -> error.getField() + error.getDefaultMessage() + ",值:" + error.getRejectedValue())
               .collect(Collectors.joining(","));
             return new ResponseEntity<>(new ErrorResponseBody(400, message), HttpStatus.BAD_REQUEST);
+        }
+        if (e instanceof HttpMessageNotReadableException) {
+            return new ResponseEntity<>(new ErrorResponseBody(400, "缺少数据体"), HttpStatus.BAD_REQUEST);
         }
         if (e.getCause() != null) {
             return handler(e.getCause());
